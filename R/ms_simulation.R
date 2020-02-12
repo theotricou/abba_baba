401e06dc47d57af48f2ba479fe6618e3cdcc9972
theotricou <t.tricou@gmail.com>
2020-02-12 17:10:33 +0100

    simple run

--- R/ms_simulation.R
+++ R/ms_simulation.R
@@ -1,20 +1,10 @@
 #!/usr/bin/env Rscript
 # Théo
-args = commandArgs(trailingOnly=TRUE)
 
+# args = commandArgs(trailingOnly=TRUE)
 
-# test if there is at least one argument: if not, return an error
-if (length(args)==0) {
-  stop("At least one argument must be supplied (ms command file).n", call.=FALSE)
-} else if (length(args)==1) {
-  # default output file
-  args[2] = "output"
-}
-
-require('coala')
-require('phyclust')
-
-activate_ms(priority = 600)
+source("ms_command.R")
+library(phangorn)
 
 is_abba <- function(seg_site) {if (seg_site[1] == seg_site[4] & seg_site[2] == seg_site[3] & seg_site[1] != seg_site[2]) {return(1)} else {return(0)}}
 
@@ -26,18 +16,33 @@ D_stat <- function(stat_simulation, quatuor){
   abba <- sum(apply(sites[c(quatuor[1],quatuor[2],quatuor[3],quatuor[4]),], 2, function(x) is_abba(x)))
   baba <- sum(apply(sites[c(quatuor[1],quatuor[2],quatuor[3],quatuor[4]),], 2, function(x) is_baba(x)))
   if ((abba + baba) != 0) {D = (abba - baba) / (abba + baba)} else {D = 0}
-  data <- list("quatuor" = c(named(quatuor[1]), named(quatuor[2]), named(quatuor[3]), named(quatuor[4])), "D-stat" = D, "abba" = abba, "baba" = baba)
-  return(data)}
+  data <- c("P1" = named(quatuor[1]), "P2" = named(quatuor[2]), "P3" = named(quatuor[3]), "P4" = named(quatuor[4]), "abba" = abba, "baba" = baba, "Dstat" = D)
+  return(data)
+}
 
-file = args[1]
-command <- readChar('ms_command.txt', file.info('ms_command.txt')$size)
-command_exe = parse(text = command)
-model <- eval(command_exe)
-pop <- eval(parse(text = strsplit(strsplit(command, ", loci")[[1]][1], " = ")[[1]][2]))
+is_H_true <- function(test, tree) {
+  a_P1 <- c(which(spnd == named(test[[1]])), Ancestors(tree, which(spnd == named(test[[1]])), type = "all"))
+  a_P2 <- c(which(spnd == named(test[[2]])), Ancestors(tree, which(spnd == named(test[[2]])), type = "all"))
+  a_P3 <- c(which(spnd == named(test[[3]])), Ancestors(tree, which(spnd == named(test[[3]])), type = "all"))
+  first_son_P3 <- a_P3[match(mrca.phylo(tree, c(which(spnd == named(test[[3]])), which(spnd == named(test[[2]])))), a_P3) - 1]
+  if (test[[7]] < 0) {
+    first_son_P1 <- a_P1[match(mrca.phylo(tree, c(which(spnd == named(test[[1]])), which(spnd == named(test[[2]])))), a_P1) - 1]
+    if (true_donor %in% Descendants(tree, first_son_P3, type = "all") &
+        true_recip %in% a_P1 ||
+        true_donor %in% Descendants(tree, first_son_P1, type = "all") &
+        true_recip %in% a_P3) {return("True")} else {return("False")}
+  } else if (test[[7]] > 0) {
+    first_son_P2 <- a_P2[match(mrca.phylo(tree, c(which(spnd == named(test[[1]])), which(spnd == named(test[[2]])))), a_P2) - 1]
+    if (true_donor %in% Descendants(tree, first_son_P3, type = "all") &
+        true_recip %in% a_P2 ||
+        true_donor %in% Descendants(tree, first_son_P2, type = "all") &
+        true_recip %in% a_P3) {return("True")} else {return("False")}
+  } else {return("False")}
+}
+
 for (i in 2:length(pop)) {pop[i] <- pop[i-1] + pop[i]}
 
-rep = simulate(model, nsim = 10000, core = 1)
-
+rep = simulate(model, nsim = 50000, core = 4)
 uniq <- lapply(rep, function(x){
   if (ncol(x$seg_sites[[1]][[1]]) == 1) {
       return(x$seg_sites[[1]][[1]])}})
@@ -45,17 +50,56 @@ uniq <- lapply(rep, function(x){
 sites <- c()
 for (i in 1:length(uniq)) {
   if (length(uniq[[i]]) != 0) {
-    if (length(sites) == 0) {
-      sites <- cbind(uniq[[i]])
-    } else {
-      sites <- cbind(sites, uniq[[i]])}}}
+    sites <- cbind(sites, uniq[[i]])}}
 
-comb = combn(seq(from = 1, to = nrow(sites)),4) # retunr all possible quatuor (only leaves)
+tree <- read.tree("Species_tree")
+spnd<-c(tree$tip.label, tree$node.label)
+dist_to_root <- dist.nodes(tree)[,7]
 
-results = as.data.frame(do.call(rbind, apply(comb, 2, function(x) D_stat(sites, x))))
+l1=l2=l3=l4 = seq(from = 1, to = nrow(sites))
+library("data.table")
+comb <- t(CJ(l1,l2,l3,l4)[l1 != l2 & l1 != l3 & l1 != l4 & l2 != l3 & l2 != l4 & l3 != l4])
+rem <- c()
+for (i in 1:ncol(comb)) {
+  if (! dist_to_root[mrca.phylo(tree, c(named(comb[1,i]), named(comb[2,i])))] > dist_to_root[mrca.phylo(tree, c(named(comb[2,i]), named(comb[3,i])))] ||
+  ! dist_to_root[mrca.phylo(tree, c(named(comb[2,i]), named(comb[3,i])))] > dist_to_root[mrca.phylo(tree, c(named(comb[3,i]), named(comb[4,i])))]) {
+    rem <- c(rem, i)
+}}
+comb_f = t(unique(apply(comb[,-rem], 2, function(x) {return(c(min(x[c(1,2)]), max(x[c(1,2)]), x[3], x[4]))})))
+topologies <- t(unique(comb_f[duplicated(comb_f),]))
+results <- as.data.frame(t(apply(topologies, 2, function(x) D_stat(sites, x))))
 
 # now test if basic H1 is true: P3 introgressed in P2 (D > 0 or P1 (D < 1)
+true_donor = which(spnd == name_donor) # from variable in sourced file
+true_recip = which(spnd == name_recip) # from variable in sourced file
+results$IsH1 <- apply(results, 1, function(x) is_H_true(x ,tree))
+print(results)
 
 
 
-.
+
+#
+#
+#
+# for (i in 1:nrow(results)){
+# test = results[i,]
+#   a_P1 <- c(which(spnd == named(test[[1]])), Ancestors(tree, which(spnd == named(test[[1]])), type = "all"))
+#   a_P2 <- c(which(spnd == named(test[[2]])), Ancestors(tree, which(spnd == named(test[[2]])), type = "all"))
+#   a_P3 <- c(which(spnd == named(test[[3]])), Ancestors(tree, which(spnd == named(test[[3]])), type = "all"))
+#   first_son_P3 <- a_P3[match(mrca.phylo(tree, c(which(spnd == named(test[[3]])), which(spnd == named(test[[2]])))), a_P3) - 1]
+#   if (test[[7]] < 0) {
+#     first_son_P1 <- a_P1[match(mrca.phylo(tree, c(which(spnd == named(test[[1]])), which(spnd == named(test[[2]])))), a_P1) - 1]
+#     if (true_donor %in% Descendants(tree, first_son_P3, type = "all") &
+#         true_recip %in% a_P1 ||
+#         true_donor %in% Descendants(tree, first_son_P1, type = "all") &
+#         true_recip %in% a_P3) {print("True")} else {print("False")}
+#   } else if (test[[7]] > 0) {
+#     first_son_P2 <- a_P2[match(mrca.phylo(tree, c(which(spnd == named(test[[1]])), which(spnd == named(test[[2]])))), a_P2) - 1]
+#     if (true_donor %in% Descendants(tree, first_son_P3, type = "all") &
+#         true_recip %in% a_P2 ||
+#         true_donor %in% Descendants(tree, first_son_P2, type = "all") &
+#         true_recip %in% a_P3) {print("True")} else {print("False")}
+#   } else {print("False")}
+# }
+#
+# apply(results, 1, function(x) is_H_true(x ,tree))
--- python3/build_ms_command.py
+++ python3/build_ms_command.py
@@ -8,21 +8,22 @@ import sys
 from ete3 import Tree as tr
 import random
 
-tree = sys.argv[1]
+# tree = sys.argv[1]
 tree = "tree"
 
 t = tr(tree, format = 1)
 
 name = 1
-t.gen = n_genaration_to_root
 Ne = 100000
 mu = 1e-7
 n_loci = 1
 len_locus = 1
 ploidy = 1
 n_genaration_to_root = 1600000
+t.gen = n_genaration_to_root
+
 ext_lineages = [] # list used for coal_model
-Coal = Merge = Migration_starts = Migration_ends = Stat_sum = str()
+Head = D_R = Pop = Coal = Merge = Migration_starts = Migration_ends = Stat_sum = str()
 
 for i in t.traverse('postorder'):
     i.add_features(gen = 0)
@@ -40,35 +41,53 @@ for i in t.traverse('postorder'):
 
 # randomly choose a donor lineage and a recipient (this recipient need to have a least one extant descendant)
 def any_descendant_alive(node):
-    for i in node.get_leaves():
-        if int(ext_lineages[int(i.name) - 1]) == 1:
-            return(True)
-            break
-        else:
-            return(False)
-
-pop_donor = pop_recip = 0
-while pop_recip == 0:
-    pop_donor = random.choice(t.get_descendants())
-    if any_descendant_alive(pop_donor) == False:
-        pop_donor = random.choice(t.get_descendants())
+    if node == False:
+        return(False)
     else:
-        pop_recip = random.choice(t.search_nodes(name = pop_donor.name)[0].get_sisters())
+        for i in node.get_leaves():
+            if int(ext_lineages[int(i.name) - 1]) == 1:
+                return(True)
+                break
+            else:
+                return(False)
 
-range_migration = [min(pop_donor.get_ancestors()[0].gen, pop_recip.get_ancestors()[0].gen), max(pop_donor.gen, pop_recip.gen)]
-range_migration # range during which the migration is possible, migration can't past a population merge
+def search_alive_togethere(tree, node):
+    "Finds nodes alive at the same time as the donor"
+    matches = []
+    for n in tree.iter_descendants('postorder'):
+        if n == node:
+            pass
+        else:
+            if node.gen <= n.gen < node.up.gen or node.gen < n.up.gen <= node.up.gen:
+                matches.append(n)
+    return(matches)
+
 
 migration_generation = 10000 # number of generation during which th population migrate
 migration_fraction = 0.1 # fraction of the donor population that migrate in recipient population during migration_generation generation
 migration_time = migration_generation / (4 * Ne) # time of the migration in 4Ne
 migration_rate = migration_fraction / migration_time # miogration rate for ms given the fraction and length
-migration_start = round(min(range_migration), 9) # time at which migrattion star given the donor et the length of the migration
-migration_end = round(migration_start + migration_time, 9) # time at which the migration stop
 
+pop_donor = pop_recip = False
+#pop_donor = random.choice(t.get_descendants())
+while any_descendant_alive(pop_recip) == False or migration_generation > ((range_migration[0] * 4 * Ne) - (range_migration[1] * 4 * Ne) + (migration_generation / 2)):
+    pop_donor = random.choice(t.get_descendants())
+    pop_recip = random.choice(search_alive_togethere(t, pop_donor))
+    range_migration = [min(pop_donor.get_ancestors()[0].gen, pop_recip.get_ancestors()[0].gen), max(pop_donor.gen, pop_recip.gen)] # range during which the migration is possible, migration can't past a population merge
+
+
+migration_start = round(min(range_migration), 9) + ((migration_generation / 2) / (4 * Ne)) # time at which migrattion star given the donor et the length of the migration
+migration_end = round(migration_start + migration_time, 9) # time at which the migration stop
 mutation_rate = len_locus * 4 * Ne * mu
 
 
-Coal = "coal_model(sample_size = c(%s), loci_number = %s, loci_length = %s, ploidy = %s) + \n" % (', '.join(ext_lineages), n_loci, len_locus, ploidy)
+Head = "require('coala') \n" + "require('phyclust') \n" + "activate_ms(priority = 600) \n"
+
+Pop = "pop <- c(%s) \n" % ', '.join(ext_lineages)
+
+D_R = "name_donor <- '%s' \n name_recip <- '%s' \n" % (pop_donor.name, pop_recip.name)
+
+Coal = "model <- coal_model(sample_size = c(%s), loci_number = %s, loci_length = %s, ploidy = %s) + \n" % (', '.join(ext_lineages), n_loci, len_locus, ploidy)
 
 Mutation = "feat_mutation(rate = %s, model = 'IFS', fixed_number = FALSE, locus_group = 'all') + \n" % mutation_rate
 
@@ -80,5 +99,7 @@ Stat_sum = "sumstat_seg_sites() + sumstat_trees() \n"
 
 t.write(outfile = 'Species_tree', format=1, features=["gen"], format_root_node=True)
 
-with open('ms_command.txt', "w+") as output:
-    output.write("".join([Coal, Mutation, Merge, Migration_starts, Migration_ends, Stat_sum]))
+with open('ms_command.R', "w+") as output:
+    output.write("".join([Head, Pop, D_R, Coal, Mutation, Merge, Migration_starts, Migration_ends, Stat_sum]))
+
+print(pop_donor.name, pop_recip.name)
